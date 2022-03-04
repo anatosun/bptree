ae7895f61c046a6db86b59d3d403cfcb5c6ab6ed
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-03-04 14:19:50 +0100

    Few tests

--- kv_test.go
+++ kv_test.go
@@ -1,13 +1,49 @@
 package kv
 
-import "testing"
+import (
+	"math/rand"
+	"sort"
+	"testing"
+)
 
-func SampleTest(t *testing.T) {
+var store kv
+var array []int
 
-	var kv kv
+const size = 1000
 
-	kv.Insert(10, Value{10, 20})
-	kv.Remove(10)
-	kv.Scan(10, 10)
-	kv.Search(10)
+func TestInit(t *testing.T) {
+	array = make([]int, 0, size)
+
+	for i := 0; i < size; i++ {
+		array = append(array, rand.Int())
+	}
+
+}
+
+func TestInsert(t *testing.T) {
+	t.Logf("inserting %d random keys", size)
+
+	for i := 0; i < size; i++ {
+		err := store.Insert(Key(array[i]), Value{byte(array[i])})
+		if err != nil {
+			t.Errorf("while inserting to kv store(%d): %v", i, err)
+			t.FailNow()
+		}
+	}
+	t.Logf("success")
+}
+func TestSearchX(t *testing.T) {
+	// sort original array
+	sort.Ints(array)
+
+	for i := 0; i < len(array); i++ {
+		k := Key(array[i])
+
+		_, err := store.Search(k)
+
+		if err != nil {
+			t.Errorf("searching err: %v", err)
+			t.Fail()
+		}
+	}
 }
