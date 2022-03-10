6747f7e53035aa6bfc956d379591bb19ca9a51c9
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-03-10 15:44:20 +0100

    Blank implementations

--- kv.go
+++ kv.go
@@ -7,5 +7,37 @@ type kv interface {
 	Insert(Key, Value) error
 	Remove(Key) (*Value, error)
 	Search(Key) (*Value, error)
+	Min() (*Key, error)
+	Max() (*Key, error)
 	Scan(Key, Key) ([]*Value, error)
+}
+
+type KV struct {
+}
+
+func (store *KV) Insert(key Key, value Value) error {
+	return nil
+}
+
+func (store *KV) Remove(key Key) (*Value, error) {
+	return nil, nil
+
+}
+
+func (store *KV) Search(key Key) (*Value, error) {
+	return nil, nil
+
+}
+
+func (store *KV) Min() (*Key, error) {
+	return nil, nil
+}
+
+func (store *KV) Max() (*Key, error) {
+	return nil, nil
+}
+
+func (store *KV) Scan(key1, key2 Key) ([]*Value, error) {
+	return nil, nil
+
 }
