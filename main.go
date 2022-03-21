a26b788cb472adfff4b44ae6f0c5e78f856c97ce
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-03-21 18:44:53 +0100

    correction of init

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"dmds-lab-01/kv"
-	"fmt"
 )
 
 func main() {
-
-	store := kv.New()
-	err := store.Insert(kv.Key(10), kv.Value{byte(10)}, false)
-	if err != nil {
-		fmt.Println("already an error? nothing is implemented...")
-	} else {
-		fmt.Println("this is a mock main, it basically does nothing...")
+	size := 100
+	store := kv.New(3)
+	for i := 0; i < size; i++ {
+		err := store.Insert(kv.Key(i), kv.Value{byte(i)})
+		if err != nil {
+			panic(err)
+		}
 	}
+
 }
