4c3ecf658b8ae8651728d3b9440d010cace162d7
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-03-25 10:01:42 +0100

    insertion utils

--- kv/insertion.go
+++ kv/insertion.go
@@ -1,57 +1,95 @@
 package kv
 
-func (b *Bplustree) splitNode(n *node) error {
+func (b *Bplustree) insert(e *entry) error {
 
-	parent := n.parent
-	median := n.median()
-	medianEntry := n.entries[median]
-	halfEntries := n.splitEntries(median)
-	halfChildren := n.splitChildren(median)
-
-	s, err := n.nextChildAt()
-	n.size = s
-	if err != nil {
-		return err
+	if b.root == nil {
+		b.root = newNode(b.degree)
+		b.root.appendEntry(e)
 	}
 
-	sibling := newNode(b.degree)
-	sibling.entries = halfEntries
-	sibling.children = halfChildren
-	for _, c := range halfChildren {
-		if c != nil {
-			c.parent = sibling
-		}
-	}
-	sibling.right = n.right
+	if b.root.full() {
 
-	if sibling.right != nil {
-		sibling.right.left = sibling
-	}
-
-	n.right = sibling
-	sibling.left = n
-
-	// if the node is root
-	if parent == nil {
-		entries := make([]*entry, 0, b.degree)
-		entries[0] = medianEntry
-		newRoot := newNode(b.degree)
-		newRoot.entries = entries
-		newRoot.appendChild(n)
-		newRoot.appendChild(sibling)
-		b.root = newRoot
-		n.parent = newRoot
-		sibling.parent = newRoot
-	} else {
-		parent.insertEntry(medianEntry)
-
-		childAt, err := parent.findIndexOfChild(n)
-		if err != nil {
+		root := newNode(b.degree)
+		right := newNode(b.degree)
+		old := b.root
+		root.children = append(root.children, old)
+		b.root = root
+		if err := b.split(root, old, right, 0); err != nil {
 			return err
 		}
-		childAt++
-		parent.insertChildAt(sibling, childAt)
-		sibling.parent = parent
+
+	}
+
+	return b.place(b.root, e)
+}
+
+func (b *Bplustree) place(n *node, e *entry) error {
+
+	if n.isLeaf() {
+		at, found := n.search(e.key)
+
+		if found {
+			return n.update(at, e.value)
+
+		}
+
+		return n.insertEntryAt(at, e)
+	}
+
+	return b.path(n, e)
+}
+
+func (b *Bplustree) path(n *node, e *entry) error {
+	at, found := n.search(e.key)
+
+	if found {
+		at++
+	}
+
+	child := n.children[at]
+
+	if child.full() {
+		sib := newNode(b.degree)
+
+		if err := b.split(n, child, sib, at); err != nil {
+			return err
+		}
+
+		if e.key >= n.entries[at].key {
+			child = n.children[at+1]
+
+		}
+	}
+
+	return b.place(child, e)
+}
+
+func (b *Bplustree) split(p, n, sibling *node, i int) error {
+	if n.isLeaf() {
+		// split leaf node. use 'sibling' as the right node for 'n'.
+		sibling.next = n.next
+		sibling.prev = n
+		n.next = sibling
+
+		sibling.entries = make([]*entry, 0, b.degree-1)
+		sibling.entries = append(sibling.entries, n.entries[b.degree:]...)
+		n.entries = n.entries[:b.degree]
+		p.insertChildAt(i+1, sibling)
+		p.insertEntryAt(i, sibling.entries[0])
+	} else {
+		// split internal node. use 'sibling' as left node for 'n'.
+		parentKey := n.entries[b.degree-1]
+
+		sibling.entries = make([]*entry, 0, b.degree-1)
+		sibling.entries = append(sibling.entries, n.entries[:b.degree]...)
+		n.entries = n.entries[b.degree:]
+
+		sibling.children = make([]*node, 0, b.degree)
+		sibling.children = append(sibling.children, n.children[:b.degree]...)
+		n.children = n.children[b.degree:]
+
+		p.insertChildAt(i, sibling)
+		p.insertEntryAt(i, parentKey)
 	}
 
 	return nil
