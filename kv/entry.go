9128975526b0579284e63dba8fb278ef51cd2187
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-03-25 10:01:24 +0100

    simple entry

--- kv/entry.go
+++ kv/entry.go
@@ -1,21 +1,9 @@
 package kv
 
-import "fmt"
+type Key uint64
+type Value [10]byte
 
 type entry struct {
 	key   Key
 	value Value
-}
-
-func (e *entry) print() {
-	fmt.Printf("%d", e.key)
-}
-
-func printEntries(entries []*entry) {
-	fmt.Printf("[ ")
-	for _, e := range entries {
-		e.print()
-		fmt.Printf(" ")
-	}
-	fmt.Printf("]")
 }
