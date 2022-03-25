5251fbd744f121fca10896f8e9bcfe66db5118b0
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-03-25 10:00:25 +0100

    flushed main

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"dmds-lab-01/kv"
-)
-
 func main() {
-	size := 100
-	store := kv.New(3)
-	for i := 0; i < size; i++ {
-		err := store.Insert(kv.Key(i), kv.Value{byte(i)})
-		if err != nil {
-			panic(err)
-		}
-	}
 
 }
