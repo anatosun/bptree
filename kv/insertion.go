435c00af3721b7797eeaeb070553255cd5b10d07
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-03-25 14:31:22 +0100

    change of naming scheme

--- kv/insertion.go
+++ kv/insertion.go
@@ -14,7 +14,7 @@ func (b *Bplustree) insert(e *entry) error {
 		old := b.root
 		root.children = append(root.children, old)
 		b.root = root
-		if err := b.split(root, old, right, 0); err != nil {
+		if err := b.split(old, right, root, 0); err != nil {
 			return err
 		}
 
@@ -51,7 +51,7 @@ func (b *Bplustree) path(n *node, e *entry) error {
 	if child.full() {
 		sib := newNode(b.degree)
 
-		if err := b.split(n, child, sib, at); err != nil {
+		if err := b.split(child, sib, n, at); err != nil {
 			return err
 		}
 
@@ -64,32 +64,33 @@ func (b *Bplustree) path(n *node, e *entry) error {
 	return b.place(child, e)
 }
 
-func (b *Bplustree) split(p, n, sibling *node, i int) error {
-	if n.isLeaf() {
-		// split leaf node. use 'sibling' as the right node for 'n'.
-		sibling.next = n.next
-		sibling.prev = n
-		n.next = sibling
+func (b *Bplustree) split(left, middle, right *node, i int) error {
+	if left.isLeaf() {
+		middle.next = left.next
+		middle.prev = left
+		left.next = middle
 
-		sibling.entries = make([]*entry, 0, b.degree-1)
-		sibling.entries = append(sibling.entries, n.entries[b.degree:]...)
-		n.entries = n.entries[:b.degree]
-		p.insertChildAt(i+1, sibling)
-		p.insertEntryAt(i, sibling.entries[0])
+		middle.entries = make([]*entry, 0, b.degree-1)
+		middle.entries = append(middle.entries, left.entries[b.degree:]...)
+		left.entries = left.entries[:b.degree]
+
+		right.insertChildAt(i+1, middle)
+		right.insertEntryAt(i, middle.entries[0])
+
 	} else {
-		// split internal node. use 'sibling' as left node for 'n'.
-		parentKey := n.entries[b.degree-1]
 
-		sibling.entries = make([]*entry, 0, b.degree-1)
-		sibling.entries = append(sibling.entries, n.entries[:b.degree]...)
-		n.entries = n.entries[b.degree:]
+		parentKey := left.entries[b.degree-1]
+		middle.entries = make([]*entry, 0, b.degree-1)
+		middle.entries = append(middle.entries, left.entries[:b.degree]...)
+		left.entries = left.entries[b.degree:]
 
-		sibling.children = make([]*node, 0, b.degree)
-		sibling.children = append(sibling.children, n.children[:b.degree]...)
-		n.children = n.children[b.degree:]
+		middle.children = make([]*node, 0, b.degree)
+		middle.children = append(middle.children, left.children[:b.degree]...)
+		left.children = left.children[b.degree:]
 
-		p.insertChildAt(i, sibling)
-		p.insertEntryAt(i, parentKey)
+		right.insertChildAt(i, middle)
+		right.insertEntryAt(i, parentKey)
+
 	}
 
 	return nil
