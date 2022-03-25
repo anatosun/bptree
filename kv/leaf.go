41c1fe6818f58f74cf7bdd82af6a342dc9ad29f2
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-03-25 10:02:26 +0100

    leaf utils

--- kv/leaf.go
+++ kv/leaf.go
@@ -2,110 +2,97 @@ package kv
 
 import (
 	"fmt"
-	"math"
 )
 
-func newLeaf(degree int) *node {
-
-	return &node{entries: make([]*entry, 0, degree), num: 0, isLeaf: true, degree: degree, parent: nil, children: make([]*node, 0, degree), size: 0, max: degree - 1, min: int(math.Ceil(float64(degree)/2) - 1), left: nil, right: nil, next: nil}
+func (n *node) isLeaf() bool {
+	return len(n.children) == 0
 }
 
-func (n *node) insertEntry(e *entry) error {
+// func (n *node) insertEntry(e *entry) error {
 
-	if e == nil {
-		return fmt.Errorf("entry is nil")
-	}
+// 	for i, entry := range n.entries {
+// 		if entry.key > e.key {
+// 			return n.insertEntryAt(i, e)
+// 		}
+// 	}
 
-	if n.full() {
-		return fmt.Errorf("node is full")
-	}
+// 	return n.appendEntry(e)
+// }
 
-	if n.empty() {
-		n.entries = append(n.entries, e)
-		n.num++
-		return nil
-	}
-
-	for i, k := range n.entries {
-
-		if e.key < k.key {
-			temp := append([]*entry{e}, n.entries[i:]...)
-			n.entries = append(n.entries[0:i], temp...)
-			n.num++
-			return nil
-
-		}
-	}
-
-	n.entries = append(n.entries, e)
-	n.num++
+func (n *node) insertEntryAt(at int, e *entry) error {
+	n.entries = append(n.entries[0:at], append([]*entry{e}, n.entries[at:]...)...)
 	return nil
 }
 
-func (n *node) stuffEntry(e *entry) error {
-	for i, k := range n.entries {
-
-		if e.key < k.key {
-			temp := append([]*entry{e}, n.entries[i:]...)
-			n.entries = append(n.entries[0:i], temp...)
-			n.num++
-			return nil
-		}
-	}
-	n.entries = append(n.entries, e)
-	n.num++
+func (n *node) update(at int, value Value) error {
+	n.entries[at].value = value
 	return nil
 }
 
-func (n *node) median() int {
+func (n *node) removeEntryAt(at int) (*entry, error) {
+	prior := len(n.entries)
+	entry := n.entries[at]
+	n.entries = append(n.entries[0:at], n.entries[at+1:]...)
+	after := len(n.entries)
 
-	return int(math.Ceil(float64(n.degree+1)/2) - 1)
-}
-
-func (n *node) deleteEntryAt(at int) error {
-
-	if n.empty() {
-		return fmt.Errorf("node is empty")
-	}
-	n.entries = append(n.entries[:at], n.entries[at+1:]...)
-	n.num--
-	return nil
-}
-
-func (n *node) splitEntries(split int) []*entry {
-	entries := n.entries
-	halfEntries := make([]*entry, 0, n.degree)
-	for i := split + 1; i < n.num; i++ {
-		halfEntries = append(halfEntries, entries[i])
-		n.deleteEntryAt(i)
+	if prior != after+1 {
+		return entry, fmt.Errorf("error deleting entry")
 	}
 
-	return halfEntries
+	return entry, nil
 }
 
-func (n *node) nextEntryAt() int {
-	return n.num
-}
-
-func (n *node) binarySearch(key Key) *entry {
-
+func (n *node) search(key Key) (int, bool) {
 	lower := 0
-	upper := n.num
-	med := lower + int((upper-lower)/2)
+	upper := len(n.entries) - 1
 
-	for lower >= upper {
+	var cursor int
+	for lower <= upper {
+		cursor = (upper + lower) / 2
+		cmp := n.entries[cursor].key
 
-		if key == n.entries[med].key {
-			return n.entries[med]
-		} else if key < n.entries[med].key {
-			upper = med - 1
-			med = lower + int((upper-lower)/2)
-		} else if key > n.entries[med].key {
-			lower = med + 1
-			med = lower + int((upper-lower)/2)
+		if cmp == key {
+			return cursor, true
+		} else if key > cmp {
+			lower = cursor + 1
+		} else if key < cmp {
+			upper = cursor - 1
 		}
 	}
 
+	return lower, false
+}
+
+func (n *node) appendEntry(e *entry) error {
+	n.entries = append(n.entries, e)
 	return nil
+}
+
+func (n *node) scan(leaf *node, at int, fn func(key Key) bool) ([]*Value, error) {
+	const INITIAL_SIZE int = 10
+	values := make([]*Value, 0, INITIAL_SIZE)
+
+	if at > len(leaf.entries)-1 {
+		return values, nil
+	}
+
+	for !fn(leaf.entries[at].key) {
+
+		values = append(values, &leaf.entries[at].value)
+		if at < len(leaf.entries)-1 {
+			at++
+		} else {
+			leaf = leaf.next
+			at = 0
+		}
+	}
+
+	if at > len(leaf.entries)-1 {
+		return values, nil
+	}
+
+	values = append(values, &leaf.entries[at].value)
+
+	return values, nil
 
 }
