b7075a97a3ceda95a9c6e01b950d7fda0fae232f
Albin <me@albxn.com>
2022-03-30 17:01:13 +0200

    modified page

--- bm/page.go
+++ bm/page.go
@@ -1,21 +1,26 @@
+package bm
+
+import (
+	"fmt"
+)
+
 //CONFIG: set here the desired pageSize in bytes
 const pageSize = 256
 
 type Page struct {
-	id int
-	data [pageSize]byte
-	dirty bool
+	id         int
+	data       [pageSize]byte
+	dirty      bool
 	pinCounter int
 }
 
 func (page *Page) getID() int {
-	return page.id;
+	return page.id
 }
 
 func (page *Page) getPinCounter() int {
-	return page.pinCounter;
+	return page.pinCounter
 }
-
 
 func (page *Page) increasePinCounter() error {
 	//possibly implement limit on pins here
@@ -27,13 +32,12 @@ func (page *Page) decreasePinCounter() error {
 	//possibly implement limit on pins here
 	if page.pinCounter != 0 {
 		panic(fmt.Errorf("page.go: Counter is already zero"))
-	}
-	else {
+	} else {
 		page.pinCounter--
 		return nil
 	}
 }
 
 func (page *Page) isDirty() bool {
-	return page.dirty;
+	return page.dirty
 }
