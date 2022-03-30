7a2cb5ab7ee20fb0f17b3f34c77b46b70dc1c50d
Albin <me@albxn.com>
2022-03-30 22:06:53 +0200

    removed assertequal, as it can be found in utils

--- bm/ring_test.go
+++ bm/ring_test.go
@@ -49,12 +49,3 @@ func TestRingOperations(t *testing.T) {
     AssertEqual(t, ring.IsFull(), true)
 
 }
-
-//https://gist.github.com/samalba/6059502
-// AssertEqual checks if values are equal
-func AssertEqual(t *testing.T, a interface{}, b interface{}) {
-    if a == b {
-        return
-    }
-    t.Errorf("Received %v (type %v), expected %v (type %v)", a, reflect.TypeOf(a), b, reflect.TypeOf(b))
-}
