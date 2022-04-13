a654bbe652574efea92356f33ee4691d2eaf24d9
Albin <me@albxn.com>
2022-04-13 20:09:06 +0200

    removed no longer needed fcts

--- kv/memory.go
+++ kv/memory.go
@@ -27,59 +27,6 @@ func findSequentialFreeSpace(free []uint64, n int) (id uint64, remaining []uint6
 	return id, free, nil
 }
 
-func (tree *BPlusTree) nodeRef(id uint64) (*node, error) {
-	// Bridge between buffer pool and bplustree
-	//fmt.Printf("Give me node wiht id=%d, length=%d\n", id, len(tree.nodes))
-	n, found := tree.nodes[id]
-	if found {
-		return n, nil
-	}
-
-	n = newNode(id)
-	// if err := tree.pager.Unmarshal(id, n); err != nil {
-	// 	return nil, err
-	// }
-	// FETCH PAGE WITH ID
-	n.dirty = false
-	tree.nodes[n.id] = n
-
-	return n, nil
-}
-
-func (tree *BPlusTree) allocate_old(n int) ([]*node, error) {
-
-	//First, let's get an free ID from this
-	pid, rem, err := findSequentialFreeSpace(tree.meta.free, n)
-	tree.meta.free = rem
-
-	if err != nil {
-		// SequentialFreeSpace is exhausted, get from here
-		// var err error
-		// pid, err = tree.pager.Alloc(n)
-		// if err != nil {
-		// 	return nil, err
-		// }
-		return nil, fmt.Errorf("not yet implemented")
-	}
-
-	nodes := make([]*node, n)
-	for i := 0; i < n; i++ {
-		//fmt.Printf("creating node with id=%d\n", pid)
-		n := newNode(pid)
-		tree.nodes[pid] = n
-		nodes[i] = n
-		pid++
-
-	}
-
-	for i := 0; i < n; i++ {
-		//node := tree.bpm.GetNewNode(3)
-		//fmt.Printf("node=%v\n",node)
-	}
-
-	return nodes, nil
-}
-
 func (tree *BPlusTree) allocate() (NodeID, error) {
 	id, err := tree.bpm.GetNewNode()
 	if err != nil {
@@ -92,31 +39,3 @@ func (tree *BPlusTree) allocate() (NodeID, error) {
 
 	return id, nil
 }
-
-// write queries the bufferpool manager to write the node to disk
-
-// uncomment this function once the bufferpool implements the proper methods
-// func (bpt *BPlusTree) write() error {
-
-// 	for _, node := range bpt.nodes {
-// 		if node.dirty {
-
-// 			if err := bpt.bufferpool.Marshal(node); err != nil {
-// 				return err
-// 			}
-
-// 			node.dirty = false
-// 		}
-// 	}
-// 	if bpt.meta.dirty {
-
-// 		if err := bpt.bufferpool.Marshal(bpt.meta); err != nil {
-// 			return err
-// 		}
-
-// 		bpt.meta.dirty = false
-
-// 	}
-
-// 	return nil
-// }
